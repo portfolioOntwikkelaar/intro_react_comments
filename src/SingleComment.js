4ec7b47f489f8b56fb248772f57a4d0047132029
noordseoostende <voropayev1@mail.ru>
2022-01-15 21:11:08 +0100

    usercard en navbar

--- src/SingleComment.js
+++ src/SingleComment.js
@@ -1,90 +1,36 @@
 import React from 'react'
-import profile4 from './image/nav.jpeg'
-import profile1 from './image/eva.jpeg'
-import profile2 from './image/john.jpeg'
-import profile3 from './image/mike.jpeg'
+// import profile4 from './image/nav.jpeg'
+// import profile1 from './image/eva.jpeg'
+// import profile2 from './image/john.jpeg'
+// import profile3 from './image/mike.jpeg'
 
-function SingleComment() {
+function SingleComment(props) {
   return (
-    <>
+    
+    
     <div className='comment'>
         <a href="/" className='avatar'>
-          <img src={profile4} alt="profile picture" />
+          <img src={props.picture} alt="profile picture" />
 
         </a>
         <div className='content'>
           <a href="/" className='author'>
-            Femke
+            {props.name}
           </a>
           <div className='metadata'>
             <span className='date'>
-              Vandag om 15:00PM
+              {props.date}
             </span>
           </div>
           <div className='text'>
-            Doe het in React!
+            {props.text}
           </div>
         </div>
       </div>
 
-<div className='comment'>
-<a href="/" className='avatar'>
-  <img src={profile1} alt="profile picture" />
 
-</a>
-<div className='content'>
-  <a href="/" className='author'>
-    Femke
-  </a>
-  <div className='metadata'>
-    <span className='date'>
-      Vandag om 5:00PM
-    </span>
-  </div>
-  <div className='text'>
-    Wonderbaarlijk
-  </div>
-</div>
-</div>
-<div className='comment'>
-<a href="/" className='avatar'>
-  <img src={profile2} alt="profile picture" />
 
-</a>
-<div className='content'>
-  <a href="/" className='author'>
-    Femke
-  </a>
-  <div className='metadata'>
-    <span className='date'>
-      Vandag om 5:00PM
-    </span>
-  </div>
-  <div className='text'>
-    Wonderbaarlijk
-  </div>
-</div>
-</div>
-<div className='comment'>
-<a href="/" className='avatar'>
-  <img src={profile3} alt="profile picture" />
-
-</a>
-<div className='content'>
-  <a href="/" className='author'>
-    Femke
-  </a>
-  <div className='metadata'>
-    <span className='date'>
-      Vandag om 5:00PM
-    </span>
-  </div>
-  <div className='text'>
-    Ik werk als ui/ux designer 
-  </div>
-</div>
-</div>
-</>  )
+  )
 }
 
 export default SingleComment
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SingleComment from './SingleComment';
+import profile4 from './image/nav.jpeg'
+import profile1 from './image/eva.jpeg'
+import profile2 from './image/john.jpeg'
+import profile3 from './image/mike.jpeg'
+import UserCard from './UserCard';
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -10,7 +15,41 @@ import SingleComment from './SingleComment';
 const App = () => {
   return(
     <div className='ui comments'>
-      <SingleComment />
+      <div class="ui pointing menu">
+  <a class="active item">
+    Home
+  </a>
+  <a class="item">
+    Messages
+  </a>
+  <a class="item">
+    Friends
+  </a>
+  <div class="right menu">
+    <div class="item">
+      <div class="ui transparent icon input">
+        <input type="text" placeholder="Search..." />
+        <i class="search link icon"></i>
+      </div>
+    </div>
+    
+  </div>
+  
+</div>
+
+<div class="ui segment">
+  <UserCard title='actress' >
+    <div>Hi, My name is Eva</div>
+<SingleComment name='Eva Longoria'  date='Today at 6:30PM'        text='Een briljante prestatie van vertaler Henri Beyle'  picture={profile1} /></UserCard>
+<UserCard title='schrijver' >
+      <SingleComment name='Femke Desmett'  date='Today at 3:22PM'  text='Enkele van de meest enervende minuten uit de wereldliteratuur staan beschreven in Henris verhaal'  picture={profile4} /></UserCard>
+      <UserCard title='politicus' >
+      <SingleComment  name='Jack Wouters'  date='Today at 11:40PM'  text='Hij vertelt in Promenades dans Rome hoe een geleerde uit zijn dagen Jupiter Feretrius abusievelijk had vertaald met Jupiter en Feretrius'  picture={profile2}/></UserCard>
+      <UserCard title='bisnessman' >
+      <SingleComment  name='Michel Dupont'  date='Today at 19:10PM'  text='De grootste verhaal van de filosoof.'  picture={profile3}/></UserCard>
+  <p></p>
+</div>
+      
       
     
     </div>
